1bd66ca1c604d9fa44e828d281229ef276472635
xiaoman <752453143@qq.com>
2019-02-14 10:48:52 +0800

    保存目前进度

---
+++ home/principalBBS/principal_forum.js
@@ -0,0 +1,22 @@
+function loadInfomation () {
+  loadAjax()
+}
+function loadAjax () {
+  var xmlhttp;
+  if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    xmlhttp = new ActiveXObject("Microsoft.XNLHTTP");
+  }
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+      console.log(xmlhttp.responseText);
+    } else {
+      console.log(xmlhttp.status)
+    }
+  }
+  xmlhttp.open("GET","backPrincipalForum/get",true);
+  xmlhttp.send();
+}
+
+window.onload = loadInfomation()
